f96f5d3049fd0925bd430c03a305c7f6638c9914
Jose Von Chong <javch1995@gmail.com>
2023-12-05 04:46:12 -0500

    optimizing into grid view and ol semantic, must check md viewport

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,13 @@
   import "../app.css";
 </script>
 
-<div class="container mx-auto py-16">
-  <div class="flex !gap-16 md:!gap-24 flex-wrap xs:justify-center sm:justify-center md:justify-center ">
+<div class="container mx-auto pt-32">
+
+      <ol class="grid xs:grid-cols-1 xs:grid-rows-1 xs:gap-12 sm:grid-cols-1 sm:grid-rows-1 sm:gap-12 md:grid-cols-2 md:grid-rows-1 md:gap-12 lg:grid-cols-2 lg:grid-rows-1 lg:gap-16 lg:justify-items-center  ">
      <slot />
-  </div>
-      <p class="!font-body-normal !font-sans !text-14/20 !text-center pt-[4]">*Aplican restricciones, consulte nuestros <a
+     </ol>
+  
+      <p class="!font-body-normal !font-sans !text-14/20 !text-center !pt-16">*Aplican restricciones, consulte nuestros <a
       href="terminos-y-condiciones#destinoDelMes" target="_blank" rel="noopener"><span class=" !text-primary hover:!underline" >T&eacute;rminos y
           condiciones</span></a>.</p>
 </div>
