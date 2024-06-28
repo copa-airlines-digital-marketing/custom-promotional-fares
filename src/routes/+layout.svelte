95f7334d54e867e5de18d2b5d2ea7903c0361d02
Jose Von Chong <javch1995@gmail.com>
2024-06-28 17:32:33 -0500

    trying moving data parameters to li tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,11 @@
   import "../app.css";
 </script>
 
-<div class="container !mx-auto pt-32">
-      <ol class="grid xs:grid-cols-1 xs:grid-rows-1 xs:gap-12 sm:grid-cols-1 sm:grid-rows-1 sm:gap-12 md:grid-cols-2 md:grid-rows-1 md:gap-12 lg:grid-cols-2 lg:grid-rows-1 lg:gap-16 lg:!justify-items-center grid-flow-row justify-items-center">
-     <slot />
+<div class="container !mx-auto">
+  <ol class="grid xs:grid-cols-1 xs:grid-rows-1 xs:gap-12 sm:grid-cols-1 sm:grid-rows-1 sm:gap-12 md:grid-cols-2 md:grid-rows-1 md:gap-12 lg:grid-cols-2 lg:grid-rows-1 lg:gap-16 lg:!justify-items-center grid-flow-row">
+         <slot />
      </ol>
-  
+</div>
       <p class="!font-body-normal !text-grey-600 !font-sans !text-14/20 !text-center !pt-16">*Aplican restricciones, consulte nuestros <a
       href="terminos-y-condiciones#destinoDelMes" target="_blank" rel="noopener"><span class=" !text-primary hover:!underline" >T&eacute;rminos y
           condiciones</span></a>.</p>
-</div>
